b65a9e1b2a9514750f7ed45cbdd13c1428ab3bf5
KhamdamovDilmurod <hamdamovdilmurod1925@gmail.com>
2024-11-27 09:21:57 +0500

    initiall commit

---
+++ lib/future_wait.dart
@@ -0,0 +1,18 @@
+
+void main() async {
+  final sw = Stopwatch()..start(); // 1
+  // await doSomeImportantWork();
+  // await doSomeImportantWork();
+  // await doSomeImportantWork();
+  await Future.wait([
+    doSomeImportantWork(),
+    doSomeImportantWork(),
+    doSomeImportantWork(),
+  ]);
+  sw.stop();
+  print('Total time: ${sw.elapsedMilliseconds}ms'); // 2
+}
+Future<void> doSomeImportantWork() async {
+  await Future.delayed(Duration(seconds: 3));
+  print('Important work done');
+}
